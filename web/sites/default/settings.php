2cee0fe4afc9a2fa9133958627ff51c10fd8bc89
Yohan TILLIER <ytillier@actualys.com>
2016-11-16 10:16:12 +0000

    Add module config_installer + reset settings.php to default.settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -244,7 +244,8 @@
  *   );
  * @endcode
  */
-$config_directories['sync'] = '../config_hc/sync';
+$config_directories = array();
+
 /**
  * Settings:
  *
@@ -284,7 +285,7 @@ $config_directories['sync'] = '../config_hc/sync';
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = 'mQofBoVF5gjaiD-Nodm039nU18kHsqygGq-mwFyEbC26XDeiUK0TzJPi5R0hsJrtXbYKt8y6bQ';
+$settings['hash_salt'] = '';
 
 /**
  * Deployment identifier.
@@ -737,17 +738,6 @@ $settings['file_scan_ignore_directories'] = [
   'node_modules',
   'bower_components',
 ];
-
-$databases['default']['default'] = array (
-  'database' => 'D8PREPROD',
-  'username' => 'root',
-  'password' => '92485cb108ffa4aa310a6669f8bea0ed0125bfbc5a70db93',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
 
 /**
  * Load local development override configuration, if available.
